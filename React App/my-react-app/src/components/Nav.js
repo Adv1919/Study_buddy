89c850acd8227b5a5984f3ac698af8c8bb4ebd1d
Advika <advikatiwari192005@gmail.com>
2024-02-17 18:35:11 +0530

    changes and changes

--- React App/my-react-app/src/components/Nav.js
+++ React App/my-react-app/src/components/Nav.js
@@ -1,7 +1,25 @@
-const Nav = () => {
-    return(
-        <div>Nav</div>
-    )
-}
+import whiteLogo from "../images/logo1.webp";
+import colorLogo from "../images/logo2.webp";
 
+const Nav = ({ authToken }) => {
+    const minimal = false
+
+  return (
+    <nav>
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={minimal ? colorLogo : whiteLogo}
+          alt="logo"
+        />
+      </div>
+      {!authToken && !minimal && <button className="nav-button">
+        Log in </button>
+      }
+    </nav>
+  );
+};
 export default Nav
+
+
+
--- React App/my-react-app/src/pages/Home.js
+++ React App/my-react-app/src/pages/Home.js
@@ -10,16 +10,17 @@ const Home = () => {
     }
 
     return(
-        <>
-        <Nav/>
+        <div className="Overlay">
+        <Nav minimal={false} authToken={authToken} />
         <div className="home">
             <h1>Swipe Right</h1>
             <button className="Primary-button" onClick={handleclick}>
                 {authToken ?'Signout':'Create Account'} 
             </button>
         </div>
-        </>
+        </div>
     )
 }
 
 export default Home
+
